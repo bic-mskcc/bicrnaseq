9073442ad8151abe058ac94e944a99d58136ed5c
Caitlin Jones <caitjbyrne@gmail.com>
2017-06-29 16:13:18 -0400

    updated tests

--- tests/testthat/test_plot_data.R
+++ tests/testthat/test_plot_data.R
@@ -2,14 +2,13 @@ library("testthat")
 
 context("Validation of plot function input data")
 
-test_that("errors from line chart data are thrown properly", {
+test_that("Errors from line chart data are handled properly", {
 
   ## two correct data frames
   c1 <- rep(1:3)
   dat <- data.frame(c1,c1,c1,c1)
   c2 <- rep(4:6)
   dat2 <- data.frame(c2,c2,c2,c2)
-
   expect_silent(bic.check.line.chart.data(dat,dat2=dat2)) 
 
   ## column 1 of data set 2 is unsorted
@@ -48,7 +47,7 @@ test_that("errors from line chart data are thrown properly", {
 })
 
 
-test_that("errors from read distribution data are thrown properly",{
+test_that("errors from read distribution data are handled properly",{
   dat <- c1 <- rep(1:3)
   dat <- data.frame(c1,c1,c1,c1)
   expect_error(bic.check.read.distribution.data(dat),
@@ -59,7 +58,7 @@ test_that("errors from read distribution data are thrown properly",{
 })
 
 
-test_that("errors from collectrnaseqmetrics data frames are thrown properly",{
+test_that("errors from PICARD metrics data frames are handled properly",{
   ad <- data.frame("SAMPLE"=c("s1","s2","s3"),
                  "RIBOSOMAL_BASES"=rep(1:3),
                  "CODING_BASES"=rep(1:3),
@@ -73,23 +72,35 @@ test_that("errors from collectrnaseqmetrics data frames are thrown properly",{
                      "MEDIAN_3PRIME_BIAS"=rep(1:3),
                      "MEDIAN_5PRIME_TO_3PRIME_BIAS"=rep(1:3)
                     )
-  dat <- bias
+  as <- data.frame("CATEGORY"=c("SECOND_OF_PAIR","hello","FIRST_OF_PAIR"),
+                   "SAMPLE"=c("s1","s2","s3"),
+                   "PF_READS"=c(1,2,3),
+                   "PF_READS_ALIGNED"=c(4,5,6))
   ## test valid bias data
-  expect_silent(bic.check.collectrnaseqmetrics.data(dat,"5prime3prime.bias"))
-  dat <- ad
+  dat <- bias
+  expect_silent(bic.check.picard.data(dat,"5prime3prime.bias"))
   ## test valid alignment distribution data
-  expect_silent(bic.check.collectrnaseqmetrics.data(dat,"alignment.distribution"))
+  dat <- ad
+  expect_silent(bic.check.picard.data(dat,"alignment.distribution"))
+  ## test valid alignment summary data
+  dat <- as
+  expect_silent(bic.check.picard.data(dat,"alignment.summary"))
 
   ## data contains non-numeric value(s)
-  dat2 = dat
+  dat2 = ad
   dat2[1,2] = "test.non.numeric"
-  expect_error(bic.check.collectrnaseqmetrics.data(dat2,"alignment.distribution"),
+  expect_error(bic.check.picard.data(dat2,"alignment.distribution"),
                regexp = "data set contains non-numeric values")
 
   ## data is missing required columns
-  dat3 = dat[,-c(3,6)] 
-  expect_error(bic.check.collectrnaseqmetrics.data(dat3,"alignment.distribution"),
+  dat3 = bias[,-c(3,6)] 
+  expect_error(bic.check.picard.data(dat3,"alignment.distribution"),
                "alignment distribution data is missing the following columns: CODING_BASES, INTERGENIC_BASES")       
+
+  ## alignment summary data is missing required column value under "CATEGORY"
+  dat4 <- as[-1,]
+  expect_error(bic.check.picard.data(dat4,"alignment.summary"),
+               "'CATEGORY' column must contain at least one instance of 'FIRST_IN_PAIR' and one of 'SECOND_IN_PAIR'")
 })
 
 
